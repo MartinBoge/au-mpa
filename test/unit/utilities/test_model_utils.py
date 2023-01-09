270dd28088eeba67177341cfd91c4d45a777ffc1
Martin Bøge <>
2023-01-10 00:10:57 +0100

    fix: test workflow skip model utils

--- test/unit/utilities/test_model_utils.py
+++ test/unit/utilities/test_model_utils.py
@@ -1,21 +1,39 @@
 import unittest
 
-import pyomo.environ as pyomo  # noqa
-from pyomo.opt import SolverStatus  # noqa
+import pyomo.environ as pyomo
+import pytest
+from pyomo.opt import SolverStatus
 
-from mpa.utilities.model_utils import solve_model  # noqa
+from mpa.utilities.model_utils import solve_model
 
 
-# To be implemented. Requires solver to be installed to test vm
+@pytest.mark.skip(
+    reason="To be implemented. Requires solver to be installed to test vm"
+)
 class TestModelUtils(unittest.TestCase):
     def test_solve_model_with_gurobi(self):
-        self.assertEqual(True, True)
+        # Create a simple Pyomo ConcreteModel
+        model = pyomo.ConcreteModel()
+        model.x = pyomo.Var()
+        model.obj = pyomo.Objective(expr=model.x)
+        model.constraint = pyomo.Constraint(expr=model.x >= 1)
+
+        # Solve the model using the Gurobi solver
+        results = solve_model(model, solver="gurobi")
+
+        # Check that the solver status is ok
+        self.assertEqual(results.solver.status, SolverStatus.ok)
+
+        # Check that the optimal solution was found
+        self.assertEqual(
+            results.solver.termination_condition, pyomo.TerminationCondition.optimal
+        )
 
     def test_solve_model_with_timelimit(self):
-        self.assertEqual(True, True)
+        ...
 
     def test_solve_model_with_mipgap(self):
-        self.assertEqual(True, True)
+        ...
 
 
 if __name__ == "__main__":
